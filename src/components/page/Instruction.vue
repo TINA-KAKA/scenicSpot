ef1d3cc9eff5327e251669d3312ad74ba6d01c7b
TINA-KAKA <tina_cookaka@163.com>
2019-04-30 17:51:27 +0800

    update

--- src/components/page/Instruction.vue
+++ src/components/page/Instruction.vue
@@ -1,6 +1,77 @@
 <template>
   <div id="instruction">
-    instruction
+    <div class="synopsis-div">
+      <div class="banner">
+        <img src="../../../static/img/banner.png" alt="">
+      </div>
+      <div class="jieshao">
+        <p class="synopsis-p1">黄鹤楼 &nbsp; AAAAA景区</p>
+        <p class="synopsis-p2">
+          <span class="synopsis-p2-span1">景区开放时间：</span><span>a 8:00-18:00（4月1日至10月1日）；</span>
+
+        </p>
+        <p class="synopsis-p2">
+          <span class="synopsis-p2-span1"></span><span class="synopsis-p2-span2">b 8:00-17:00（11月-1日至3月31日）</span>
+        </p>
+        <p class="synopsis-p3">
+          <i class="iconfont icon-iconfontzhizuobiaozhun16"></i> 湖北省武汉市长江南岸武昌蛇山之巅
+        </p>
+        <p class="synopsis-p4">
+          <a href="picture.html" class="synopsis-p4-span1"><i class="iconfont icon-zhaoxiangji f20"></i><span >相关图片</span></a>
+          <a class="synopsis-p4-span2"><i class="iconfont icon-jianjie"></i><span>景点简介</span></a>
+        </p>
+      </div>
+      <div class="zhegai"></div>
+      <div class="tanchu">
+        <h3>景点简介</h3>
+        <p>黄鹤楼位于湖北省武汉市长江南岸的武昌蛇山之巅，是国家5A级旅游景区 自古享有“天下江山第一楼”和“天下绝景”</p>
+        <p>黄鹤楼位于湖北省武汉市长江南岸的武昌蛇山之巅，是国家5A级旅游景区 自古享有“天下江山第一楼”和“天下绝景”</p>
+      </div>
+      <div class="ticket">
+        <h2>景点门票</h2>
+        <ul>
+          <li>
+            <div>
+              <p class="ticket-p1"><span class="huanhang">黄鹤楼成人票</span> <span class="fl-r c-red">￥75 <span class="span80">￥80</span></span></p>
+              <p class="ticket-p2"><span class="biaoqian"><span class="lan">15:00前可预订今日票</span><span class="ju">条件退</span></span><a href="#">在线付</a><div class="clean"></div></p>
+              <p class="ticket-p3">票型说明 <i class="iconfont icon-arrow_r f12"></i></p>
+            </div>
+          </li>
+          <li>
+            <div>
+              <p class="ticket-p1"><span class="huanhang">黄鹤楼学生票</span> <span class="fl-r c-red">￥40 <span class="span80">￥80</span></span></p>
+              <p class="ticket-p2"><span class="biaoqian"><span class="lan">15:00前可预订今日票</span><span class="ju">条件退</span><span class="hong">出示学生证</span></span><a href="#">在线付</a><div></div></p>
+              <p class="ticket-p3">票型说明 <i class="iconfont icon-arrow_r f12"></i></p>
+            </div>
+          </li>
+          <li>
+            <div>
+              <p class="ticket-p1"><span class="huanhang">武汉森泰中洋酒店+黄鹤楼/武汉两江游览(夜游长江)</span> <span class="fl-r c-red">￥499<span class="span80"></span><span class="c-gary">起</span></span></p>
+              <p class="ticket-p2"><a href="#">套餐详情</a><div class="clean"></div>  </p>
+              <p class="ticket-p3"></p>
+            </div>
+          </li>
+          <li>
+            <div>
+              <p class="ticket-p1"><span class="huanhang">黄鹤楼/武汉两江游览(夜游长江)+武汉凯莱熙酒店</span> <span class="fl-r c-red">￥547<span class="span80"></span><span class="c-gary">起</span></span></p>
+              <p class="ticket-p2"><a href="#">套餐详情</a><div class="clean"></div></p>
+              <p class="ticket-p3"></p>
+            </div>
+          </li>
+        </ul>
+      </div>
+      <div class="comment">
+        <h2>游客点评 <i class="iconfont icon-arrow_r f12 fl-r c-gary"></i></h2>
+        <ul>
+          <li>
+            <p class="comment-p1"><img src="../../../static/img/head01.png" alt=""><span>用户5451856</span><span class="fl-r c-yellow"><i class="iconfont icon-xingxing1"></i><i class="iconfont icon-xingxing1"></i><i class="iconfont icon-xingxing1"></i><i class="iconfont icon-xingxing1"></i></span></p>
+            <p class="time">2017-12-25</p>
+            <p class="comment-p2">出游之前看过攻略，武汉必游之地就是黄鹤楼！但是因为酒店距离黄鹤楼有点 远，所以道德时候已经十点多了，取票的时候也有很多人，因为里面是景区...</p>
+            <p  class="comment-p3"><img src="../../../static/img/comm01.png" ><img src="../../../static/img/comm02.png" ><img src="../../../static/img/comm03.png" ></p>
+          </li>
+        </ul>
+      </div>
+    </div>
 
   </div>
 </template>
