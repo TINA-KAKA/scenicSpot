4971ff928df33f535c8be90723dd1b85b10bf41b
TINA-KAKA <tina_cookaka@163.com>
2019-04-30 17:47:29 +0800

    six

--- src/components/page/PictureList.vue
+++ src/components/page/PictureList.vue
@@ -3,28 +3,28 @@
     <div class="picture-div">
       <ul>
         <li>
-          <img src="img/picture01.png" alt="">
+          <img src="../../../static/img/picture01.png" alt="">
         </li>
         <li>
-          <img src="img/picture02.png" alt="">
+          <img src="../../../static/img/picture02.png" alt="">
         </li>
         <li>
-          <img src="img/picture03.png" alt="">
+          <img src="../../../static/img/picture03.png" alt="">
         </li>
         <li>
-          <img src="img/picture04.png" alt="">
+          <img src="../../../static/img/picture04.png" alt="">
         </li>
         <li>
-          <img src="img/picture05.png" alt="">
+          <img src="../../../static/img/picture05.png" alt="">
         </li>
         <li>
-          <img src="img/picture06.png" alt="">
+          <img src="../../../static/img/picture06.png" alt="">
         </li>
         <li>
-          <img src="img/picture07.png" alt="">
+          <img src="../../../static/img/picture07.png" alt="">
         </li>
         <li>
-          <img src="img/picture08.png" alt="">
+          <img src="../../../static/img/picture08.png" alt="">
         </li>
       </ul>
     </div>
